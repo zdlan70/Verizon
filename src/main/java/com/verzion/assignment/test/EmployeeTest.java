a7b05b92295ec531052603c3378b6eb61755cf22
zdlan70 <zlan@hotmail.com>
2016-06-06 17:13:02 -0400

    Create EmployeeTest.java

---
+++ src/main/java/com/verzion/assignment/test/EmployeeTest.java
@@ -0,0 +1,11 @@
+package com.verzion.assignment.test;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+public class EmployeeTest {
+	@Test
+	public void test() {
+		SpringApplication.run(EmployeeTest.class);
+	}
+}
