c13e2ddd650def008096d4b3e546473a070d03e3
zdlan70 <zlan@hotmail.com>
2016-06-06 17:12:41 -0400

    Create DepartmentTest.java

---
+++ src/main/java/com/verzion/assignment/test/DepartmentTest.java
@@ -0,0 +1,15 @@
+package com.verzion.assignment.test;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+import com.verzion.assignment.DepartmentApplication;
+
+public class DepartmentTest {
+
+	@Test
+	public void test() {
+		SpringApplication.run(DepartmentApplication.class);
+	}
+
+}
