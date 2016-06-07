35bf3f39f18c459779f75a6e085f2f3f4f15fa5a
zdlan70 <zlan@hotmail.com>
2016-06-07 16:41:36 -0400

    Update Application.java

--- src/main/java/com/verzion/assignment/Application.java
+++ src/main/java/com/verzion/assignment/Application.java
@@ -17,7 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws Exception {
-		if (args.length > 0) { // testRest
+		if (args.length > 1) { // testRest
 			testRest();
 		} else { // standalone
 			SpringApplication.run(DepartmentApplication.class);
